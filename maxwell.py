e02ce5dc65cce40e54099a7a114c400022e83472
MScroggs <matthew.w.scroggs@gmail.com>
2017-01-31 15:33:13 +0000

    Added evaluator for direct methods

--- maxwell.py
+++ maxwell.py
@@ -63,6 +63,23 @@ def cfie_evaluator(solution, k, eta, incident=None):
             plot_me = incident(points) - eta * (elec * solution) - (magn * solution)
             return plot_me
     return evaluator
+
+def direct_evaluator(solution, k, incident=None):
+    if incident is None:
+        raise ValueError("Direct formulation requires an incident wave.")
+    else:
+        from bempp.api import GridFunction
+        import numpy as np
+        def tangential_trace(x, n, domain_index, result):
+            result[:] = np.cross(incident(x), n, axis=0)
+        incident_fun =  GridFunction(solution.space, fun=tangential_trace)
+        def evaluator(points):
+            from bempp.api.operators.potential import maxwell
+            magn = maxwell.magnetic_field(solution.space, points, k)
+            elec = maxwell.electric_field(solution.space, points, k)
+            plot_me = incident(points) - (elec * solution) + (magn * incident_fun)
+            return plot_me
+    return evaluator
     
 
 
@@ -78,4 +95,7 @@ def cfie_slice(solution, k, eta, plot_type='squared', incident=None, *args, **kw
 
 def rcfie_slice(solution, k, eta, M, plot_type='squared', incident=None, *args, **kwargs):
     plot_slice(rcfie_evaluator(solution, k, eta, M, incident), plot_type, *args, **kwargs)
+
+def direct_slice(solution, k, plot_type='squared', incident=None, *args, **kwargs):
+    plot_slice(direct_evaluator(solution, k, incident), plot_type, *args, **kwargs)
 
